4b332786974f503e945ab982290ff849fb64a898
Hari Paudel <109645425+hp977@users.noreply.github.com>
2022-07-31 13:20:52 +0545

    added file from remote repository
    used drag and drop to add file to remote repository

---
+++ script.js
@@ -0,0 +1,20 @@
+let unhide =  document.querySelector('#unhide');
+
+unhide.addEventListener('click', () => {
+
+    if (block.style.display = "block") {
+        block.style.display = "none"
+    }else {
+        block.style.display = "block"
+    }
+    ;
+
+})
+
+
+
+let block = document.querySelector('.hidden_nav_menu')
+
+block.addEventListener('click', () => {
+
+})
